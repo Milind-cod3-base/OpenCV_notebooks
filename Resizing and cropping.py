f85d4b5be472e2f3eef968f8cd73457f74ea6452
Milind <milindchaudhary80@gmail.com>
2021-09-16 21:24:46 +0530

    Cropping has been performed

--- Resizing and cropping.py
+++ Resizing and cropping.py
@@ -2,7 +2,7 @@
 #frame's origin is taken at the left topmost corner.
 
 import cv2
-import numpy as np
+import numpy as np   #because image is just an array/matrix of pixels and for cropping, we can eliminate few elements
 
 ad = cv2.imread('Resources/audi.png')
 print(ad.shape)   #prints in height, width and channels(BGR) format
@@ -10,6 +10,8 @@ print(ad.shape)   #prints in height, width and channels(BGR) format
 adResize = cv2.resize(ad,(700,400))   #here width, height format comes.
 print(adResize.shape)
 
+adCropped = ad[0:300, 500:910]
 cv2.imshow('audi',ad)
-cv2.imshow('audi resized',adResize)
+#cv2.imshow('audi resized',adResize)       #this print is made silent, getting output for crop only.
+cv2.imshow('audi cropped',adCropped)
 cv2.waitKey(0)
